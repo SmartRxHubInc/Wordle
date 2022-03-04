4bdc1796d7083aa453a9474996d598f83a43ce6e
Ashish Panchal <asis.dynamic.methods@gmail.com>
2022-03-04 18:47:04 +0530

    Splash screen
    - Solve splash screen delay issue

--- app/src/main/java/com/dms/wordle/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/dms/wordle/ui/splash/SplashActivity.kt
@@ -2,7 +2,9 @@ package com.dms.wordle.ui.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import androidx.activity.viewModels
 import com.dms.wordle.BaseActivity
@@ -12,22 +14,22 @@ import com.dms.wordle.ui.home.HomeActivity
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseActivity() {
-
     private val viewModel: SplashViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
         super.onCreate(savedInstanceState)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
         setContentView(R.layout.activity_splash)
         viewModel.getStartTime()
-        viewModel.isFinish.observe(this, {
+        viewModel.isFinish.observe(this) {
             if (it) {
                 startActivity(Intent(this, HomeActivity::class.java))
                 finish()
             }
-        })
+        }
     }
 }
